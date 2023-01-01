be20ee2feebc281641bab95a78f993a34d917d39
analia-brb <analia.brb@gmail.com>
2023-01-01 23:06:48 +0000

    Test with Jest

---
+++ app.js
@@ -0,0 +1,50 @@
+// declaramos una funcion con el mismo nombre "formEuroToDollar"
+const fromEuroToDollar = function(valueInEuro){
+    // convertimos el valor a dolares
+    let valueInDollar = valueInEuro * 1.2;
+    // retornamos el valor
+    return valueInDollar;
+}
+
+// esta es mi función que suma dos números
+const sum = (a,b) => {
+    return a + b
+}
+
+// solo un registro en consola para nosotros.
+console.log(sum(7,3))
+
+// exporta la función para usarla en otros archivos 
+// (similar a la palabra clave `export` cuando se usa webpack)
+module.exports = { sum, fromEuroToDollar };
+
+// fromYenToPound
+
+// declaramos una funcion con el mismo nombre "fromYenToPound"
+const fromYenToPound = function(valueInYen){
+    // convertimos el valor a dolares
+    let valueInPound = valueInYen * 159.875;
+    // retornamos el valor
+    return valueInPound;
+}
+
+
+// exporta la función para usarla en otros archivos 
+// (similar a la palabra clave `export` cuando se usa webpack)
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound };
+
+// fromDollarToYen
+
+// declaramos una funcion con el mismo nombre "fromDollarToYen"
+const fromDollarToYen = function(valueInDollar){
+    // convertimos el valor a dolares
+    let valueInYen = valueInDollar * 106;
+    // retornamos el valor
+    return valueInYen;
+}
+
+
+// exporta la función para usarla en otros archivos 
+// (similar a la palabra clave `export` cuando se usa webpack)
+module.exports = { sum, fromEuroToDollar, fromDollarToYen };
+
